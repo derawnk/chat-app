8cdb10f817362a930f3fe4e6242d5f33a1212afc
derawnk <derawnk@gmail.com>
2024-06-23 18:08:20 -0400

    Add initial css styling to chat section

--- src/components/chat/Chat.jsx
+++ src/components/chat/Chat.jsx
@@ -2,7 +2,29 @@ import "./chat.css"
 
 const Chat = () => {
     return (
-        <div className='chat'>Chat</div>
+        <div className='chat'>
+
+            <div className="top">
+                <div className="user">
+                    <img src="./avatar.png" alt="avatar"></img>
+                    <div className="texts">
+                        <span>User Name</span>
+                        <p>User Description Area.</p>
+                    </div>
+                </div>
+
+                <div className="icons">
+
+                <img src="./phone.png" alt="phone"></img>
+                <img src="./video.png" alt="video"></img>
+                <img src="./info.png" alt="info"></img>
+
+                </div>
+            </div>
+
+            <div className="center"></div>
+            <div className="bottom"></div>
+        </div>
     )
 }  
 
