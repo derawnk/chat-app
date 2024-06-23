ab6ba803a2e112b116aeb7a4ce9d31af620b5df1
derawnk <derawnk@gmail.com>
2024-06-23 12:55:26 -0400

    Implement chat list scroll functionality

--- src/components/list/chatList/ChatList.jsx
+++ src/components/list/chatList/ChatList.jsx
@@ -1,6 +1,8 @@
+import { useState } from "react"
 import "./chatList.css"
 
 const ChatList = () => {
+    const [addMode,setAddMode]= useState(false)
     return (
         <div className='chatList'>
             <div className="search">
@@ -8,8 +10,38 @@ const ChatList = () => {
                     <img src="/search.png" alt="search"></img>
                     <input type="text" placeholder="Search"/>
                 </div>
-                <img src="./plus.png" alt="plus"/>
+
+                <img src={addMode ? "./minus.png" : "./plus.png"} 
+                alt="plus" 
+                className="add"
+                onClick={() => setAddMode((prev) => !prev)}
+                />
             </div>
+
+            <div className="item">
+                <img src="./avatar.png" alt="avatar" />
+                <div className="texts">
+                    <span>User Name</span>
+                    <p>Hi</p>
+                </div>
+            </div>
+
+            <div className="item">
+                <img src="./avatar.png" alt="avatar" />
+                <div className="texts">
+                    <span>User Name</span>
+                    <p>Hi</p>
+                </div>
+            </div>
+
+            <div className="item">
+                <img src="./avatar.png" alt="avatar" />
+                <div className="texts">
+                    <span>User Name</span>
+                    <p>Hi</p>
+                </div>
+            </div>
+
         </div>
     )
 }  
