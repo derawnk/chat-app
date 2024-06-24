f2bc702d37b660bca885fd49bae363296df961fd
derawnk <derawnk@gmail.com>
2024-06-24 09:45:47 -0400

    Add initial css styling to message bar

--- src/components/chat/Chat.jsx
+++ src/components/chat/Chat.jsx
@@ -23,7 +23,23 @@ const Chat = () => {
             </div>
 
             <div className="center"></div>
-            <div className="bottom"></div>
+
+            <div className="bottom">
+
+                <div className="icons">
+                    <img src="./img.png" alt="img"></img>
+                    <img src="./camera.png" alt="camera"></img>
+                    <img src="./mic.png" alt="mic"></img>
+                </div>
+
+                <input type="text" placeholder="Type a message..."></input>
+                <div className="emoji">
+                    <img src="./emoji.png" alt="emoji"></img>
+                </div>
+
+                <button className="sendButton">Send</button>
+
+            </div>
         </div>
     )
 }  
