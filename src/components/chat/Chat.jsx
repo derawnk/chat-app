f2a491830f7da46e822e41f35baf73445b2b2389
derawnk <derawnk@gmail.com>
2024-06-26 14:41:03 -0400

    Add function to start chat from most recent message

--- src/components/chat/Chat.jsx
+++ src/components/chat/Chat.jsx
@@ -1,10 +1,20 @@
-import { useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import "./chat.css"
 import EmojiPicker from "emoji-picker-react";
 
 const Chat = () => {
 const [open,setOpen] = useState(false);
 const [text,setText] = useState("");
+
+const endRef = useRef(null)
+
+useEffect(() => {
+    endRef.current?.scrollIntoView({behavior: "smooth"})
+
+},[])
+
+
+
 
 const handleEmoji = e => {
     setText(prev => prev + e.emoji);
@@ -84,7 +94,12 @@ const handleEmoji = e => {
                     </div>
                 </div>
 
+                <div ref={endRef}></div>
+
+
             </div>
+
+            
 
             <div className="bottom">
 
